03a1b18c325fcbb24789bd87d1823699d59d882a
Faber Andrés Vergara Holguín <fabervergara@gmail.com>
2020-06-22 23:33:22 -0500

    add helpers folder and laravel-mix

---
+++ webpack.mix.js
@@ -0,0 +1,6 @@
+const mix = require('laravel-mix');
+
+mix.js('resources/js/app.js', 'public/js')
+  .sass('resources/sass/app.sass', 'public/css')
+  .browserSync('http://localhost/wp-test/')
+  .sourceMaps();
