0e6a6f12f000a4e716f6055272836b6f230dbdf3
Faber Andrés Vergara Holguín <fabervergara@gmail.com>
2020-06-23 12:00:21 -0500

    add containr

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
 
-<div class="flex mb-4" id="app">
-  <div class="w-1/2 h-12" :class="message.bgClass" v-for="message in messages">{{ message.text }}</div>
+<div class="container mx-auto">
+  <div class="flex mb-4" id="app">
+    <div class="w-1/2 h-12" :class="message.bgClass" v-for="message in messages">{{ message.text }}</div>
+  </div>
 </div>
 
 <?php get_footer() ?>
