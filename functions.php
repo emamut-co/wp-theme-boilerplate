e2ac6a2a691e7dcab64dd501db56ee2475bee3af
Faber Andrés Vergara Holguín <fabervergara@gmail.com>
2020-06-22 22:50:46 -0500

    update files from development

--- functions.php
+++ functions.php
@@ -4,9 +4,6 @@ require_once dirname( __FILE__ ) . '/helpers/TGM-Plugin-Activation-2.6.1/class-t
 require_once dirname( __FILE__ ) . '/helpers/required-plugins.php';
 // require_once dirname( __FILE__ ) . '/helpers/rest_custom_endpoints.php';
 
-// require_once dirname( __FILE__ ) . '/helpers/slider-cpt.php';
-// require_once dirname( __FILE__ ) . '/helpers/slider-metabox.php';
-
 add_theme_support( 'post-thumbnails' );
 
 function emamut_setup()
@@ -17,32 +14,50 @@ add_action( 'after_setup_theme', 'emamut_setup' );
 
 function add_theme_scripts()
 {
-  wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css', array(), '1.1', 'all');
-  wp_enqueue_style('fontawesome', 'https://use.fontawesome.com/releases/v5.2.0/css/all.css', array(), '1.1', 'all');
+  wp_enqueue_script('jquery', '//code.jquery.com/jquery-3.3.1.slim.min.js', array (), 1.1, true);
+  wp_enqueue_script('popper', '//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array (), 1.1, true);
+  wp_enqueue_script('bootstrap', '//stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js', array (), 1.1, true);
 
-  wp_enqueue_style('main', get_template_directory_uri() . '/assets/css/main.css');
+  wp_enqueue_style('fontawesome', '//use.fontawesome.com/releases/v5.2.0/css/all.css', array(), '1.1', 'all');
 
-  // wp_enqueue_script('angular', 'https://ajax.googleapis.com/ajax/libs/angularjs/1.7.2/angular.min.js', array (), 1.1, true);
-  // wp_enqueue_script('angular-resource', 'https://code.angularjs.org/1.7.2/angular-resource.min.js', array (), 1.1, true);
-  // wp_enqueue_script('angular-sanitize', 'https://code.angularjs.org/1.7.2/angular-sanitize.min.js', array (), 1.1, true);
+  wp_enqueue_script('vue', '//cdn.jsdelivr.net/npm/vue/dist/vue.js', array (), 1.1, true);
+  wp_enqueue_script('axios', '//cdn.jsdelivr.net/npm/axios/dist/axios.min.js', array (), 1.1, true);
 
-  wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.3.1.slim.min.js', array (), 1.1, true);
-  wp_enqueue_script('popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array (), 1.1, true);
-  wp_enqueue_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js', array (), 1.1, true);
+  wp_enqueue_style('main', get_template_directory_uri() . '/css/main.css');
 
-  // wp_enqueue_script('app.js', get_template_directory_uri() . '/assets/js/app.js', array (), 1.1, true);
-  wp_enqueue_script('helpers.js', get_template_directory_uri() . '/assets/js/helpers.js', array (), 1.1, true);
-
-  // $controllers_folder = '/assets/js/controllers/';
-  // $files = array_diff(scandir(dirname( __FILE__ ) . $controllers_folder), array('.', '..'));
-
-  // foreach($files as $key => $file)
-  //   wp_enqueue_script($file, get_template_directory_uri() . $controllers_folder . $file, array (), 1.1, true);
-
-  // $models_folder = '/assets/js/models/';
-  // $files = array_diff(scandir(dirname( __FILE__ ) . $models_folder), array('.', '..'));
-
-  // foreach($files as $key => $file)
-  //   wp_enqueue_script($file, get_template_directory_uri() . $models_folder . $file, array (), 1.1, true);
+  // wp_enqueue_script('app.js', get_template_directory_uri() . '/src/js/app.js', array (), 1.1, true);
+  wp_enqueue_script('helpers.js', get_template_directory_uri() . '/src/js/helpers.js', array (), 1.1, true);
 }
 add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+
+register_nav_menus( array(
+  'primary' => __( 'Primary Menu', 'emamut' ),
+) );
+
+function register_navwalker(){
+	require_once get_template_directory() . '/helpers/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'after_setup_theme', 'register_navwalker' );
+
+function config_custom_logo() {
+  add_theme_support( 'custom-logo' );
+}
+add_action( 'after_setup_theme' , 'config_custom_logo' );
+
+function theme_get_custom_logo() {
+  if ( has_custom_logo() ) {
+    $logo = wp_get_attachment_image_src( get_theme_mod( 'custom_logo' ) , 'full' );
+
+    echo '<img class="img-fluid" id="logo" src="' . esc_url( $logo[0] ) . '" alt="' . get_bloginfo( 'name' ) . '">';
+  }
+  else
+    echo '<h1>' . get_bloginfo( 'name' ) . '</h1>';
+}
+
+add_action('wp_head', 'myplugin_ajaxurl');
+function myplugin_ajaxurl() {
+  echo "<script type=\"text/javascript\">
+          let siteURL = '" . get_site_url() . "',
+          themePath = '" . get_template_directory_uri() ."'
+        </script>";
+}
--- header.php
+++ header.php
@@ -4,16 +4,9 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php bloginfo( 'name' ); ?></title>
+    <title><?php bloginfo('name'); wp_title(); ?></title>
 
     <?php wp_head(); ?>
   </head>
   <body>
-    <script>
-      wp_content = '<?php echo WP_CONTENT_URL ?>';
-      myTheme = '<?php echo get_template_directory_uri() ?>';
-    </script>
-
-    <?php $url = ''; if(!is_home()) $url = get_site_url(); ?>
-
-    <div ng-view>
+    <div>
