5039cdd089b5e71d5746d56f4160d899390c95ec
Faber Andrés Vergara Holguín <fabervergara@gmail.com>
2020-07-08 15:06:27 -0500

    Add logo from backend

--- partials/menu-template.php
+++ partials/menu-template.php
@@ -4,7 +4,7 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="<?php echo get_site_url() ?>"><?php echo theme_get_custom_logo(); ?></a>
       <?php
         wp_nav_menu( array(
           'theme_location'    => 'primary',
