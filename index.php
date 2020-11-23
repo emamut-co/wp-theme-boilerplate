cdcd8771a925eb0cdb8b47bf6382048123a49c92
Faber Vergara <fabervergara@gmail.com>
2020-11-23 10:50:18 -0500

    fix layout

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php get_header(); ?>
 
-  <div class="container mx-auto">
+  <div class="flex justify-center mt-3 flex-wrap">
+    <div class="w-4/5">
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+        the_content();
+      endwhile; else: ?>
+        <p>Sorry, no posts matched your criteria.</p>
+      <?php endif; ?>
+    </div>
   </div>
 
 <?php get_footer() ?>
